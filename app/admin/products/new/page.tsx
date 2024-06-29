febcbd314fbdc2ad7d36489cb08e43f281cb53dd
GlendyT <glendytuyuc@gmail.com>
2024-06-29 13:41:06 -0600

    routing y formulario para crear productos, mostrando las categorias en el formulario, mezclando componentes cliente y servidore, composicion para utilizar server components dentro de client, validando la creacion de productos, cloudinari para subir imagenes, obtener la URL de la imagen que se sube, almacenando el producto a la base de datos, como mostrar las imagenes si viene de cloudinary

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,7 +1,16 @@
+import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProductForm";
+import Heading from "@/components/ui/Heading";
 
 
-export default function page() {
+export default function CreateProductPage() {
   return (
-    <div>page</div>
+    <>
+    <Heading>Nuevo Producto</Heading>
+
+    <AddProductForm>
+      <ProductForm/>
+    </AddProductForm>
+    </>
   )
 }
---
+++ components/products/ImageUpload.tsx
@@ -0,0 +1,60 @@
+"use client"; //TODO CLOUDINARY SOLO FUNCIONA EN USE CLIENT
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useState } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+export default function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState("");
+  return (
+    <CldUploadWidget
+      onSuccess={(result, { widget }) => {
+        if(result.event === "success") {
+          widget.close()
+          // @ts-ignore
+          setImageUrl(result.info?.secure_url)
+        }
+        
+      }}
+      uploadPreset="f40ealpc"
+      options={{
+        maxFiles: 1,
+      }}
+    >
+      {({ open }) => (
+        <>
+          <div className="space-y-2">
+            <label className="text-slate-800">Imagen producto</label>
+            <div
+              className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
+              onClick={() => open()}
+            >
+              <TbPhotoPlus size={50} />
+              <p className="text-lg font-semibold">Agregar Imagen</p>
+
+              {imageUrl && (
+                <div
+                 className="absolute inset-0 w-full h-full"
+                >
+                  <Image
+                   fill
+                   style={{objectFit: "contain"}}
+                   src={imageUrl}
+                   alt="Imagen de Producto"
+                   />
+
+                </div>
+              )}
+            </div>
+          </div>
+
+          <input
+           type="hidden"
+           name="image"
+           value={imageUrl}
+          />
+        </>
+      )}
+    </CldUploadWidget>
+  );
+}
